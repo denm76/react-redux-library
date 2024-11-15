c887046d4431e9ffd0df477b48552640040ca190
denm76 <denm76@gmail.com>
2024-11-15 13:17:24 +0200

    Added Error Slice

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import booksReducer from "./slices/booksSlice.js";
 import filterReducer from "./slices/filterSlice.js";
+import errorReducer from "./slices/errorSlice.js";
 
 const store = configureStore({
   reducer: {
     books: booksReducer,
     filter: filterReducer,
+    error: errorReducer,
   },
 });
 
